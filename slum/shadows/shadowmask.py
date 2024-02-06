5600da7ca7b47d6932a6a3efd4b7d53493d9a70d
Yannick TANGUY <yannick.tanguy@cnes.fr>
2024-02-06 12:28:14 +0000

    Shadow mask with EOScale, options to erode / remove small shadows

--- slum/shadows/shadowmask.py
+++ slum/shadows/shadowmask.py
@@ -11,69 +11,108 @@ import numpy as np
 import argparse
 import os
 
-def compute_mask(image, mask, threshold_RGB, threshold_NIR, percentile):
-    ds_im= rasterio.open(image)
-    profile = ds_im.profile
-    profile.update({'compress' : 'lzw'})
-    
-    nir = ds_im.read(4)
-    
-    band = []
-    th_band = []
-    shadow_mask = np.zeros_like(nir)
-    
-    min_nir = np.percentile(nir, percentile)
-    th_nir = threshold_NIR * (np.percentile(nir, 100-percentile) - min_nir)
-        
-    shadow = nir < min_nir+th_nir
+from skimage.morphology import binary_closing, binary_opening, binary_erosion, remove_small_objects, disk, remove_small_holes
 
-    for cpt in range(1,4):
-        band = ds_im.read(cpt)
-        min_band = np.percentile(band,percentile)
-        th_band  = threshold_RGB * (np.percentile(band, 100-percentile) - min_band)
-        shadow = np.logical_and(shadow, band < min_band+th_band)            
+
+import eoscale.manager as eom
+import eoscale.eo_executors as eoexe
+
+def compute_mask(input_buffers: list, 
+                 input_profiles: list, 
+                 params: dict) -> np.ndarray :
     
-    shadow_mask[shadow] = 1
+
+    raw_shadow_mask = np.zeros(input_buffers[0][0].shape, dtype=int)
+    raw_shadow_mask.fill(1)
     
-    with rasterio.open(mask, 'w+', 
-                       driver="GTiff",
-                       compress="deflate",
-                       height=ds_im.shape[0],
-                       width=ds_im.shape[1],
-                       crs=ds_im.crs,
-                       transform=ds_im.transform,
-                       count=1, dtype=np.uint8) as mask_file:
-        mask_file.write(shadow_mask.astype(np.uint8),1)
+    for i in range(4):
+        raw_shadow_mask = np.logical_and(raw_shadow_mask, input_buffers[0][i] < params["thresholds"][i])
     
+    # work on binary arrays
+    final_shadow_mask = binary_opening(raw_shadow_mask, disk(params["binary_opening"]))
+    final_shadow_mask = remove_small_objects(final_shadow_mask, params["small_objects"], connectivity=2)
+        
+    raw_shadow_mask = np.where(raw_shadow_mask,1,0)
+    final_shadow_mask = np.where(final_shadow_mask,1,0)
+    
+    final_shadow_mask += raw_shadow_mask
+    
+    return final_shadow_mask
+
+def single_uint8_profile(input_profiles: list, map_params):
+    profile = input_profiles[0]
+    profile['count']=1
+    profile['dtype']=np.uint8
+    profile["compress"] = "lzw"
+    profile["nodata"] = 255
+    
+    return profile
+
+def getarguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image", help="Input 4 bands VHR image")
+    parser.add_argument("mask", help="Final mask")
+    parser.add_argument("-th_rgb", default=0.3, help="Relative shadow threshold for RGB bands")
+    parser.add_argument("-th_nir", default=0.3, help="Relative shadow threshold for NIR band")
+    parser.add_argument("-percentile", default=2, help="Percentile value to cut histogram and estimate shadow threshold")
+    parser.add_argument("-binary_opening","--binary_opening", type=int, required=False, default=0, action="store",
+                        help="Size of ball structuring element")
+    parser.add_argument("-remove_small_objects","--small_objects", type=int, required=False, default=0, action="store",
+                        help="The maximum area, in pixels, of a contiguous object that will be removed")
+ 
+    parser.add_argument("-n_workers",type=int, default=8, required=False, action="store", dest="nb_workers", help="Nb of CPU" )
+
+    args = parser.parse_args()
+
+    return args
 
 def main():
-    try:
-        parser = argparse.ArgumentParser()
-        parser.add_argument("image", help="Input 4 bands VHR image")
-        parser.add_argument("mask", help="Final mask")
-        parser.add_argument("-th_rgb", default=0.3, help="Relative shadow threshold for RGB bands")
-        parser.add_argument("-th_nir", default=0.3, help="Relative shadow threshold for NIR band")
-        parser.add_argument("-percentile", default=2, help="Percentile value to cut histogram and estimate shadow threshold")
-        
-        args = parser.parse_args()
+    args = getarguments()
+    
+    with eom.EOContextManager(nb_workers = args.nb_workers, tile_mode = True) as eoscale_manager:
+        try:
 
-        compute_mask(args.image, args.mask, float(args.th_rgb), float(args.th_nir), int(args.percentile))
+            # Store image in shared memmory
+            key_phr = eoscale_manager.open_raster(raster_path = args.image)
+            local_phr = eoscale_manager.get_array(key_phr)
 
-    except FileNotFoundError as fnfe_exception:
-        print("FileNotFoundError", fnfe_exception)
+            # Compute threshold for each band
+            th_bands = np.zeros(4)
+            for cpt in range(3):
+                min_band = np.percentile(local_phr[cpt],args.percentile)
+                th_bands[cpt]  = min_band + args.th_rgb * (np.percentile(local_phr[cpt], 100-args.percentile) - min_band)
+                #print(f"{cpt=} : {min_band=} {th_bands[cpt]=}")
 
-    except PermissionError as pe_exception:
-        print("PermissionError", pe_exception)
+            min_nir = np.percentile(local_phr[3], args.percentile)
+            th_bands[3] = min_nir + args.th_nir * (np.percentile(local_phr[3], 100-args.percentile) - min_nir)
+            
+            params = {"thresholds":th_bands, "binary_opening":args.binary_opening, "small_objects":args.small_objects}
 
-    except ArithmeticError as ae_exception:
-        print("ArithmeticError", ae_exception)
+            mask_shadow = eoexe.n_images_to_m_images_filter(inputs = [key_phr] ,
+                                                           image_filter = compute_mask,
+                                                           filter_parameters=params,
+                                                           generate_output_profiles = single_uint8_profile,
+                                                           stable_margin= args.small_objects,
+                                                           context_manager = eoscale_manager,
+                                                           filter_desc= "Shadow mask processing...")          
 
-    except MemoryError as me_exception:
-        print("MemoryError", me_exception)
+            eoscale_manager.write(key = mask_shadow[0], img_path = args.mask)
 
-    except Exception as exception:  # pylint: disable=broad-except
-        print("oups...", exception)
-        traceback.print_exc()
+        except FileNotFoundError as fnfe_exception:
+            print("FileNotFoundError", fnfe_exception)
+
+        except PermissionError as pe_exception:
+            print("PermissionError", pe_exception)
+
+        except ArithmeticError as ae_exception:
+            print("ArithmeticError", ae_exception)
+
+        except MemoryError as me_exception:
+            print("MemoryError", me_exception)
+
+        except Exception as exception:  # pylint: disable=broad-except
+            print("oups...", exception)
+            traceback.print_exc()
 
 
 if __name__ == "__main__":
