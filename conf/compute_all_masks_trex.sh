46e926c6242e106105dc39b7f2f6f70e6156660e
Yannick TANGUY <yannick.tanguy@cnes.fr>
2024-06-06 13:16:41 +0000

    Improve config for regularization

--- conf/compute_all_masks_trex.sh
+++ conf/compute_all_masks_trex.sh
@@ -47,7 +47,7 @@ slum_shadowmask ${TMPDIR}/image/${filename} shadows/shadowmask.tif -binary_openi
 slum_urbanmask  -watermask water/watermask.tif -vegetationmask vegetation/vegetationmask.tif  -ndvi water/watermask_NDVI.tif -ndwi water/watermask_NDWI.tif -binary_closing 5 -binary_opening 2 -shadowmask shadows/shadowmask.tif ${TMPDIR}/image/${filename} urban/urbanmask.tif -nb_samples_urban 10000 -nb_samples_other 10000 -binary_dilation 5 
 
 # Stack
-slum_stackmasks ${TMPDIR}/image/${filename} stack/stack_simple.tif -vegmask vegetation/vegetationmask.tif -watermask water/watermask.tif -waterpred water/watermask.tif -urban_proba urban/urbanmask_proba.tif  -shadow shadows/shadowmask.tif -wsf urban/wsf.tif -remove_small_objects 500 -binary_closing 3 -binary_opening 3 -remove_small_holes 500 -building_erosion 2
+slum_stackmasks ${TMPDIR}/image/${filename} stack/stack_simple.tif -vegmask vegetation/vegetationmask.tif -watermask water/watermask.tif -waterpred water/watermask.tif -urban_proba urban/urbanmask_proba.tif  -shadow shadows/shadowmask.tif -wsf urban/wsf.tif -remove_small_objects 300 -binary_closing 3 -binary_opening 3 -remove_small_holes 300 -building_erosion 2 -bonus_gt 10 -malus_shadow 10
 
 stop_monitoring.sh --name SLUM_all_masks
 
