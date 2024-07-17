40153c03bd80be4c17c878970e6cf0e23eb366a5
Raille Céline (THALES SERVICES NUMERIQUES) <celine.raille@thalesgroup.com>
2024-07-17 08:18:35 +0000

    fix primitives

--- slurp/prepare/primitives.py
+++ slurp/prepare/primitives.py
@@ -21,6 +21,6 @@ def compute_ndxi(input_buffer: list, input_profiles: list, params: dict) -> np.n
     im_ndxi[np.logical_or(im_ndxi < -1000.0, im_ndxi > 1000.0)] = np.nan
     im_ndxi[np.logical_not(input_buffer[1][0])] = np.nan
     np.nan_to_num(im_ndxi, copy=False, nan=32767)
-    im_ndvi = np.int16(im_ndxi)
+    im_ndxi = np.int16(im_ndxi)
 
     return im_ndxi
