71c39773317cbfbbd5afbb978a3196f40103883c
cadauxe <ema.cadaux@cs-soprasteria.com>
2025-08-19 15:13:19 +0200

    feat: removed wsf,pekel,hand validation from tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -51,9 +51,6 @@ def pytest_configure(config):
         pytest.features_test_img = conf["features_test_img"]
         pytest.output_dir = conf["output_dir"]
         pytest.ref_dir = conf["ref_dir"]
-        pytest.pekel = conf["pekel"]
-        pytest.hand = conf["hand"]
-        pytest.wsf = conf["wsf"]
     pytest.main_config = os.path.join(current_dir, "main_config_tests.json")
     if not os.path.exists(pytest.output_dir):
         os.makedirs(pytest.output_dir)
