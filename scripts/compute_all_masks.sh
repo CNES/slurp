36164a8f0638d2b6278cbb05bd3f23fd3de6f245
Yannick TANGUY <yannick.tanguy@cnes.fr>
2025-08-21 08:41:45 +0000

    Improve documentation of scripts

--- scripts/compute_all_masks.sh
+++ scripts/compute_all_masks.sh
@@ -3,6 +3,7 @@
 nb_args=3
 if [ $# -ne $nb_args ]; then
     echo "Launch SLURP on a 4 bands (R G B NIR) image and get simple land use mask"
+    echo "External data is retrieved and superimposed with OTB"
     echo ""
     echo "Usage : "
     echo "compute_slurp_masks.sh <MAIN_CONFIG> <PATH_TO_VHR_IMAGE> <LAND_USE_MASK>"
@@ -11,6 +12,8 @@ if [ $# -ne $nb_args ]; then
     echo "export OPT_PREPARE='sensor_mode=true'"
     echo "(idem with OPT_WATER, OPT_VEG, OPT_SHADOW, OPT_URBAN, OPT_STACK"
     echo "--"
+    echo ""
+    echo "OTB is supposed to be properly installed in your environment"
     echo ""
     exit 0
 fi
@@ -27,6 +30,8 @@ mkdir out
 echo "Launch SLURP from `pwd`"
 
 # Superimpose Pekel, Hand and WSF with OTB
+# 
+# /!\ Adapt path depending on where your global Pekel database (resp. HAND, WSF) is located
 otbcli_Superimpose -inr ${VHR_IM} -inm /work/datalake/static_aux/MASQUES/PEKEL/data2021/occurrence/occurrence.vrt -out "out/pekel.tif?&gdal:co:TILED=YES&gdal:co:COMPRESS=DEFLATE" uint8 -interpolator nn
 otbcli_Superimpose -inr ${VHR_IM} -inm /work/datalake/static_aux/MASQUES/HAND_MERIT/hnd.vrt -out "out/hand.tif?&gdal:co:TILED=YES&gdal:co:COMPRESS=DEFLATE" 
 otbcli_Superimpose -inr ${VHR_IM} -inm /work/datalake/static_aux/MASQUES/WSF/WSF2019_v1/WSF2019_v1.vrt -out "out/wsf.tif?&gdal:co:TILED=YES&gdal:co:COMPRESS=DEFLATE" uint8 -interpolator nn
