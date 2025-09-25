2e220bde20ed44277ec6b5a53b9e49b2349e2acc
cyouri-csgroup <youri.colera@csgroup.eu>
2025-09-25 14:01:35 +0200

    out_wsf & out_pekel

--- tests/test_valid/test_valid_sensor_geom.py
+++ tests/test_valid/test_valid_sensor_geom.py
@@ -105,11 +105,11 @@ def test_prepare_sensor_geom(
 ):
     """Tests the sensor geometry preparation and validates output masks."""
     for file, dtm in input_files:
-        _, _, _, wsf, pekel = prepare_sensor_geom(
+        _, _, _, out_wsf, out_pekel = prepare_sensor_geom(
             main_config, wsf, pekel, output_dir, file, dtm, 1
         )
-        validate_mask(pekel, "Sensor", ref_dir, valid_pixels=False)
-        validate_mask(wsf, "Sensor", valid_pixels=False)
+        validate_mask(out_pekel, "Sensor", ref_dir, valid_pixels=False)
+        validate_mask(out_wsf, "Sensor", valid_pixels=False)
 
 
 @pytest.mark.validation
